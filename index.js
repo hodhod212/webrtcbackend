a651d1c55fd73e9b67b38a70635f3f81ed857474
hodhod222 <aliezadkhaha212@gmail.com>
2022-11-22 14:03:49 +0100

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const io = require("socket.io")({ path: "/webrtc" });
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.get("/", (req, res) => res.send("Hello, webrtc!"));
 const server = app.listen(port, () => {
   console.log(`WebRTC App is running on port ${port}`);
